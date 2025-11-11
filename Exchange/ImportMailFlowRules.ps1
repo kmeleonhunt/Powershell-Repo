6376a11b3e551a462a267e64d70967050fe297a2
Kmeleon <64780903+kmeleonhunt@users.noreply.github.com>
2025-11-11 17:24:24 +0100

    Add files via upload

---
+++ Exchange/ImportMailFlowRules.ps1
@@ -0,0 +1,64 @@
+﻿<#
+________      _____            ______  __                    
+____  _/________  /_______________  / / /____  ____   ______ 
+ __  / __  __ \  __/  _ \_  ___/_  /_/ /__  / / /_ | / /  _ \
+__/ /  _  / / / /_ /  __/  /   _  __  / _  /_/ /__ |/ //  __/
+/___/  /_/ /_/\__/ \___//_/    /_/ /_/  _\__, / _____/ \___/ 
+                                        /____/               
+
+SCRIPT CREATED BY : THOMAS AMANN
+ 
+    .SYNOPSIS
+       Import Mail FLow rules to Tenant
+
+    .DESCRIPTION
+    This script import the mailflow rules from an XML file to Exchange Online. After importing you will have to enable the rules manually and check the settings.
+
+    .REQUIREMENTS
+    You need the StandardRuleCollectionToImport.xml file in the C:\MailFlowRuleCollections folder.    
+
+#>
+
+<#
+.FUNCTIONS
+#>
+Connect-ExchangeOnline
+
+$directoryPath = "C:\MailFlowRuleCollections"
+
+# Check if the directory exists
+if (-not (Test-Path -Path $directoryPath)) {
+    # If the directory does not exist, create it
+    New-Item -Path $directoryPath -ItemType Directory
+    Write-Output "Directory created at $directoryPath"
+} else {
+    # If the directory exists, output a message
+    Write-Output "Directory already exists at $directoryPath"
+}
+
+
+[xml]$xml = Get-Content "C:\MailFlowRuleCollections\StandardRuleCollectionToImport.xml"
+
+$rulesToImport = $xml.SelectNodes("//rules/rule")
+
+if ($rulesToImport.Count -eq 0)
+
+{
+    Write-Host "There are no mail flow rules to be imported."
+
+    return
+}
+
+Write-Host "Importing $($rulesToImport.Count) mail flow rules."
+
+$index = 0
+
+foreach ($rule in $rulesToImport)
+
+{
+    $index++
+
+    Write-Host "Importing rule '$($rule.Name)' $index/$($rulesToImport.Count)."
+
+    Invoke-Expression $($rule.version.commandBlock.InnerText) | Out-Null
+}
