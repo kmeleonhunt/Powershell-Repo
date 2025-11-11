c3f04882d0acd736de1a36eaabb477163fab3ae5
Kmeleon <64780903+kmeleonhunt@users.noreply.github.com>
2025-11-11 17:23:53 +0100

    Add files via upload

---
+++ Exchange/ExportMailFlowRules.ps1
@@ -0,0 +1,59 @@
+﻿<#
+________      _____            ______  __                    
+____  _/________  /_______________  / / /____  ____   ______ 
+ __  / __  __ \  __/  _ \_  ___/_  /_/ /__  / / /_ | / /  _ \
+__/ /  _  / / / /_ /  __/  /   _  __  / _  /_/ /__ |/ //  __/
+/___/  /_/ /_/\__/ \___//_/    /_/ /_/  _\__, / _____/ \___/ 
+                                        /____/               
+
+SCRIPT CREATED BY : THOMAS AMANN
+ 
+    .SYNOPSIS
+       Export Mail FLow rules to XML file
+
+    .DESCRIPTION
+    This script exports the mail flow rules from Exchange Online to an XML file.
+
+    .REQUIREMENTS
+        
+
+#>
+
+<#
+.FUNCTIONS
+#>
+function Export-TransportRuleXML{
+
+Connect-ExchangeOnline
+
+$Tenant = (Get-AcceptedDomain | Where-Object { $_.Default -eq $true } | Sort-Object -Property Name | Select-Object Name)
+
+Write-Host "Informal : You are currently logged in $($Tenant) - Make sure you want to run the script on $($Tenant)" -ForegroundColor Yellow
+
+function Save-File {
+    $SaveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+    $SaveFileDialog.Filter = "CSV files (*.xml)|*.xml"
+    $SaveFileDialog.FileName = "$(Get-Date -Format 'yyyyMMdd')_UserReport_$($client).xml"
+
+    if ($SaveFileDialog.ShowDialog() -eq "OK") {
+        $SaveFileDialog.FileName
+    } else {
+        "No path selected"
+    }
+}
+
+$path = Save-File
+
+$file = Export-TransportRuleCollection
+
+[System.IO.File]::WriteAllBytes($path, $file.FileData)
+
+Write-Host "O365 admin Transport Rules were exported to $($path)"
+
+}
+
+########
+#SCRIPT#
+########
+
+Export-TransportRuleXML
